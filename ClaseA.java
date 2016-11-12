ad6dd34b2277f91d3f23298d5a3dfcfe5098f96c
idlasa <ragio1@hotmail.com>
2016-11-12 07:06:03 -0600

    Modificación
    Se elimino variable z y se creo variable código.

--- ClaseA.java
+++ ClaseA.java
@@ -2,12 +2,12 @@ public class ClaseA{
 
   private int x;
   private int y;
-  private int z;
+  private string codigo;
   
   public ClaseA(){
     x = 0;
     y = 0;
-    z = 0;
+    codigo = "";
   }
   
  }
